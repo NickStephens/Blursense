6eeae996a37cdf9ebe385b5572486b99542f3165
haonguyen14 <hao_nguyen1412@yahoo.com.vn>
2013-11-25 00:29:18 -0800

    Repy Compatible

--- SecurityLayer/XMLRPCClient/BatteryRequest.py
+++
@@ -1,6 +0,0 @@
-from BlurClient import BlurClient
-
-
-class BatteryRequest (BlurClient):
-	def get ( self, params ):
-		return getattr ( getattr ( self.mRPCClient, 'BatterySensor' ), 'level' ) ()
--- SecurityLayer/XMLRPCClient/PressureRequest.py
+++
@@ -1,6 +0,0 @@
-from BlurClient import BlurClient
-
-
-class PressureRequest (BlurClient):
-	def get (self, params):
-		return getattr ( getattr ( self.mRPCClient, 'PressureSensor'), 'pressure' ) ()
